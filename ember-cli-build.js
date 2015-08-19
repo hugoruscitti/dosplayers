42aaf8cc3dd6718f88d350bcd4499a6d8300cc25
Hugo Ruscitti <hugoruscitti@gmail.com>
2015-08-19 19:30:02 -0300

    Agregando imágenes de los juegos.

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -4,7 +4,7 @@ var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 module.exports = function(defaults) {
   var app = new EmberApp(defaults, {
     fingerprint: {
-      exclude: ['work'],
+      exclude: ['work', 'images'],
     }
   });
 
